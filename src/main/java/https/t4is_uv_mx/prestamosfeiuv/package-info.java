15a8e21efbfde31ca3d4598c9cd087fcf17b3f10
Hiroshi1228 <81598809+Hiroshi1228@users.noreply.github.com>
2022-05-20 13:32:06 -0500

    make it better

--- src/main/java/https/t4is_uv_mx/prestamosfeiuv/package-info.java
+++ src/main/java/https/t4is_uv_mx/prestamosfeiuv/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
 // Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2022.05.20 a las 11:00:49 AM CDT 
+// Generado el: 2022.05.20 a las 01:20:09 PM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "https://t4is.uv.mx/prestamosFEIUV", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
